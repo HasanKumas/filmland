ccf5872201f46867faa0ac6d97aee23e1a9a41b6
HKUMAS <hasan.kumas@capgemini.com>
2020-11-16 03:19:45 +0100

    login added

--- src/main/java/com/sogeti/filmland/controllers/UserController.java
+++ src/main/java/com/sogeti/filmland/controllers/UserController.java
@@ -17,5 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("filmland") // end point
 public class UserController {
+    @Autowired // connect to database
+    private UserAccountRepository userAccountRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
 
+    /**
+     * this method let the user login
+     * by checking the requested user from the database
+     * with encoded passwords
+     * @param user
+     * @return status and message
+     */
+    @PostMapping("/login")
+    public ResponseMessage login(@RequestBody LoginRequest user) {
+        UserDetails existingUser = userService.loadUserByUsername(user.getEmail());
+
+        return validateUser(user, existingUser);
+    }
+    /**
+     * checks if login credentials match
+     * @param user
+     * @param existingUser
+     * @return status and message
+     */
+    private ResponseMessage validateUser(LoginRequest user, UserDetails existingUser) {
+        ResponseMessage responseMessage = new ResponseMessage();
+
+        if (existingUser != null && (passwordEncoder.matches(user.getPassword(), existingUser.getPassword()))) {
+            responseMessage.setStatus("Login successful!");
+            responseMessage.setMessage("Welcome to Filmland!");
+        }else
+        {
+            responseMessage.setStatus("Login failed!");
+            responseMessage.setMessage("Username or password is not correct.");
+        }
+        return responseMessage;
+    }
 }
--- src/main/java/com/sogeti/filmland/services/UserService.java
+++ src/main/java/com/sogeti/filmland/services/UserService.java
@@ -9,10 +9,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Service
+@Component
 public class UserService implements UserDetailsService {
     @Autowired // connect to database
     private UserAccountRepository userAccountRepository;
